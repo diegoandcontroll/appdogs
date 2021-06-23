006c80a519ba591f0005d096d9b6c02ca4a56bd3
Diego Lucas <lukasxdp2@gmail.com>
2021-06-22 21:02:44 -0300

    Add component PhotoComments

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -1,10 +1,20 @@
 import React from 'react';
-
-const PhotoComments = ({id, comments}) => {
+import {UserContext} from '../../UserContext';
+import PhotoCommentsForm from './PhotoCommentsForm';
+import styles from './PhotoComment.module.css';
+const PhotoComments = (props) => {
+  const [comments, setComments] = React.useState(() => props.comments);
+  const {login} = React.useContext(UserContext);
   return (
-    <div>
-      
-    </div>
+    <>
+      <ul className={styles.comments}>
+        {comments.map(comment => <li key={comment.comment_ID}>
+          <b>{comment.comment_author}:</b>
+          <span>{comment.comment_content}</span>
+        </li>)}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments}/>}
+    </>
   )
 }
 
