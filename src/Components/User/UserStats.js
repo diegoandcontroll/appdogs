514319d68a80cd07ff01e5fe64598e087054cc4a
Diego Lucas <lukasxdp2@gmail.com>
2021-06-25 13:42:55 -0300

    add stats

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import Head from '../Helper/Head';
+import useFetch from '../../Hooks/useFetch';
+import Loading from '../../Components/Helper/Loading';
+import Error from '../../Components/Helper/Error';
+import UserStatsGraphs from './UserStatsGraphs';
+import { GET_STATS } from '../../api';
 const UserStats = () => {
+  const {data, error, loading, request} = useFetch();
+  React.useEffect(() => {
+    async function getData(){
+      const {url, options} = GET_STATS();
+      await request(url, options);
+    }
+    getData();
+  }, [request])
+  if(loading) return <Loading />
+  if(error) return <Error error={error}/>
+  if(data)
   return (
     <div>
       <Head title="Estatísticas"/>
-      StatsUser
+      <UserStatsGraphs data={data}/>
     </div>
   )
+  else return null;
 }
 
 export default UserStats
